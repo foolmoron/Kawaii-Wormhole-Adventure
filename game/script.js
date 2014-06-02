30c55c205900d3a0b8f77c588531f720860a3748
Momin Khan <foolmoron@gmail.com>
2014-06-01 21:55:30 -0400

    Added linting to script declarations

--- game/script.js
+++ game/script.js
@@ -22,3 +22,51 @@ KWA.SCRIPT = [
 	{name: 'five', dialogue: "hey test5 hey test5 hey test5 hey test5 hey test5 hey test5 hey test5", func: function() { console.log('5'); }},
 	{label: 'restart', func: 'fadeOut', options: { onComplete: function() { this.state.restart(true, false, KWA.SCRIPT); } }}
 ];
+
+//lint the script lines to make sure there are no hard to find errors
+(function(script) {
+	var SCRIPT_LINE_IN_CODE = 18;
+	var VALID_PROP_NAMES = [
+		'label',
+		'name',
+		'dialogue',
+		'func',
+		'options',
+		'advance'
+	];
+
+	var labelToLineMap = {};
+	var advanceToLabels = [];
+
+	for (var i = 0; i < script.length; i++) {
+		var lineErrorPrefix = "Line " + i + " (code line " + (SCRIPT_LINE_IN_CODE + i) + ")";
+		var line = script[i];
+
+		//check for misspelled properties
+		for (var prop in line) {
+			if (VALID_PROP_NAMES.indexOf(prop) < 0) {
+				console.error(lineErrorPrefix + " contains misspelled property '" + prop + "'!");
+			}
+		}
+
+		//check for duplicate labels
+		if (line.label) {
+			if (labelToLineMap[line.label] != null) {
+				console.error(lineErrorPrefix + " has duplicate label '" + line.label + "', used by line " + labelToLineMap[line.label] + " (code line " + (SCRIPT_LINE_IN_CODE + labelToLineMap[line.label]) + ")!");
+			}
+			labelToLineMap[line.label] = i;
+		}
+
+		//get list of all jumps to specific labels
+		if (line.advance && typeof line.advance == 'string') {
+			advanceToLabels.push(line.advance);
+		}
+	}
+	//check for any jumps to non-existent labels
+	var allLabels = Object.keys(labelToLineMap);
+	for (var i = 0; i < advanceToLabels.length; i++) {
+		if (allLabels.indexOf(advanceToLabels[i]) < 0) {
+			console.error(lineErrorPrefix + " jumps to non-existent label '" + advanceToLabels[i] + "'!");
+		}
+	}
+})(KWA.SCRIPT);
