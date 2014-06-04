0854edbe33a7e7c3af397ed6f446b19a1afd213f
Momin Khan <foolmoron@gmail.com>
2014-06-03 22:59:20 -0400

    Added screenshake

--- game/functions.js
+++ game/functions.js
@@ -34,5 +34,28 @@ _.extend(KWA.fn, {
 	fadeIn: function(options) {
 		options.fadeOut = false;
 		KWA.fn.call(this, 'fade', options);
+	},
+
+	screenShake: function(options) {
+		options = _.extend({
+			magnitude: 30,
+			duration: 1000,
+			onComplete: function() {}
+		}, options);
+
+		if (this._screenShakeRecord) {
+			this._oldScreenShakeTween.stop();
+		}
+
+		var magnitudeObj = { magnitude: options.magnitude };
+		this._oldScreenShakeTween = this.add.tween(magnitudeObj)
+        	.to({magnitude: 0}, options.duration, null, true)
+        	.onUpdateCallback(function(tween, value) {
+        		var magnitude = (1 - value) * options.magnitude; // 'value' is just raw interp value, so we have to manually calculate magnitude
+				console.log(magnitude);
+				var randX = this.rnd.integerInRange(-magnitude, magnitude);
+				var randY = this.rnd.integerInRange(-magnitude, magnitude);
+    			this.world.setBounds(randX, randY, this.game.width + randX, this.game.height + randY);
+        	}, this);
 	}
 });
